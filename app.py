f8a0b73175c443eb5d9a40c09d8cecfcf9aa544c
lala9456 <61819951+lala9456@users.noreply.github.com>
2023-07-10 14:29:43 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('$Your Channel Access Token') # 必須放上自己的Channel Access Token(在line那邊按下issue)
+handler = WebhookHandler('$Your Channel Secret') # 必須放上自己的Channel Secret
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
